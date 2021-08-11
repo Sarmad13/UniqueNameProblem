6e3dcdcac0f5df5a0a7575a98ed5384d07b1499c
Sarmad Saeed <sarmadjutt4@outlook.com>
2021-08-12 00:53:15 +0500

    Add project files.

---
+++ AssignmentQ_2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace AssignmentQ_2
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string[] names1 = new string[] { "Ava", "Emma", "Olivia" };
+            string[] names2 = new string[] { "Olivia", "Sophia", "Emma" };
+            Console.WriteLine(string.Join(", ", Program.UniqueNames(names1, names2))); // should print Ava, Emma, Olivia, Sophia
+        }
+        public static string[] UniqueNames(string[] names1, string[] names2)
+        {
+            return names1.Union(names2).ToArray();
+        }
+    }
+}
